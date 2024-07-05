8f3379ddc4a70872a6db21384f7bd24d0bf44b5a
erosquesada <eros.quesada@slu.se>
2024-07-05 11:11:26 +0200

    Added according to new scheme, to be developed

---
+++ Simulators/Simulator2_SIMPLE/estimation_SIMPLE/estimation.R
@@ -0,0 +1,25 @@
+########################################################################################
+#
+# Estimation
+# 
+########################################################################################
+#
+# This script estimates the proportion and variance of a simulated flow of small 
+# pelagics, based on the flow produced in SmallPelagicFlowSim.R script and using the 
+# samples extracted in the sampling.R script.
+# 
+#
+########################################################################################
+#
+#
+# Authors:
+#
+# - 
+# 
+# Dev. Notes.
+#
+#
+# 2024-07-05: Created. 
+#
+########################################################################################
+## Below the part on the estimation
